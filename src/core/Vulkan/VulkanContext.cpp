b2e01ed760ee07c6f58804467d0b2267e20b2a6b
Asquart <70846142+Asquart@users.noreply.github.com>
2025-12-02 16:06:40 +0000

    Initialize Vulkan function pointers before querying device properties

--- src/core/Vulkan/VulkanContext.cpp
+++ src/core/Vulkan/VulkanContext.cpp
@@ -17,11 +17,17 @@ void FVulkanContext::Initialize()
     InteropDevice->GetVulkanHandles(&Instance, &PhysicalDevice, &Device);
     InteropDevice->GetSubmissionQueue(&Queue, &QueueIndex, &QueueFamilyIndex);
 
+    VulkanFunctionsHooks::InitVulkanFunctionPointers(Instance, Device);
+
+    if (!p_vkGetPhysicalDeviceProperties)
+    {
+        Logger::Log("FVulkanContext: vkGetPhysicalDeviceProperties not available");
+        return;
+    }
+
     VkPhysicalDeviceProperties deviceProps{};
     p_vkGetPhysicalDeviceProperties(PhysicalDevice, &deviceProps);
     TimestampPeriod = deviceProps.limits.timestampPeriod;
-
-    VulkanFunctionsHooks::InitVulkanFunctionPointers(Instance, Device);
 
     InitSamplers();
     InitPools();
